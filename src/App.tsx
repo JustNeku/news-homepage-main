24b90c1912fa82134291f2fa93643cd0718dc740
JustNeku <dennisbruening96@hotmail.com>
2023-08-26 11:15:32 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import './App.scss'
+import Navigation from './navigation/navigation';
+import headlineMobile from './assets/images/image-web-3-mobile.jpg';
+import headlineWeb from './assets/images/image-web-3-desktop.jpg';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [screenSize, setScreenSize] = useState(getCurrentWidth());
+
+  function getCurrentWidth(){
+    return window.innerWidth
+  }
+
+  useEffect(() => {
+    const updateWidth = () => {
+      setScreenSize(getCurrentWidth())
+    }
+    window.addEventListener('resize', updateWidth);
+    
+    return(() => {
+        window.removeEventListener('resize', updateWidth);
+    })
+  }, [screenSize])
+
+  return (
+    <>
+    <div className="wrapper">
+      <Navigation></Navigation>
+      <main>
+
+        <article className="headline">
+          <div className="headline-image">
+            <img src={`${screenSize <= 768 ? headlineMobile : headlineWeb}`} alt="web-3" />
+          </div>
+          <h1>The Bright Future of Web 3.0?</h1>
+          <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
+          But is it really fulfilling its promise?</p>
+          <button>Read more</button>
+        </article>
+
+
+        <section className="new">
+          <p className="head">New</p>
+
+          <article>
+            <a href=''>Hydrogen VS Electric Cars</a>
+            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+          </article>
+
+          <span className="spacer"></span>
+
+          <article>
+            <a href=''>The Downsides of AI Artistry</a>
+            <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+          </article>
+
+          <span className="spacer"></span>
+
+          <article>
+            <a href=''>Is VC Funding Drying Up?</a>
+            <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+          </article>
+        </section>
+
+
+        <section className="sub">
+          <article>
+            <div className="article-photo">
+              <img src="/src/assets/images/image-retro-pcs.jpg" alt="Retro PCs" />
+            </div>
+            <span className="number">01</span>
+            <a href=''>Reviving Retro PCs</a>
+            <p>What happens when old PCs are given modern upgrades?</p>
+          </article>
+
+          <article>
+            <div className="article-photo">
+              <img src="/src/assets/images/image-top-laptops.jpg" alt="Laptops" />
+            </div>
+            <span className="number">02</span>
+            <a href=''>Top 10 Laptops of 2022</a>
+            <p>Our best picks for various needs and budgets.</p>
+          </article>
+          
+          <article>
+            <div className="article-photo">
+              <img src="/src/assets/images/image-gaming-growth.jpg" alt="Retro PCs" />
+            </div>
+            <span className="number">03</span>
+            <a href=''>The Growth of Gaming</a>
+            <p>How the pandemic has sparked fresh opportunities.</p>
+          </article>
+        </section>
+      </main>
+
+
+      <footer>
+        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.</p>
+        <p>Coded by Dennis Brüning.</p>
+      </footer>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/navigation/navigation.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './navigation.scss';
+
+function Navigation() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
+
+    function toggleMenu() {
+        if(isMenuOpen) {
+            setIsClosing(true);
+            setTimeout(() => {
+                setIsClosing(false);
+                setIsMenuOpen(false);
+            }, 200);
+        }
+        else {
+            setIsMenuOpen(true);
+        }
+            
+    }
+
+    return (
+        <>
+        <div className='backdrop' data-open={`${isMenuOpen}`} data-closing={`${isClosing}`}></div>
+        <nav>
+            <div className="logo">
+                <img src="/src/assets/images/logo.svg" alt="logo" />
+            </div>
+            <div className="menu-btn-open" onClick={toggleMenu}>
+                <img src="/src/assets/images/icon-menu.svg" alt="menu-open" />
+            </div>
+            <div className="menu" data-open={`${isMenuOpen}`} data-closing={`${isClosing}`}>
+                <div className="menu-btn-close"  onClick={toggleMenu}>
+                    <img src="/src/assets/images/icon-menu-close.svg" alt="menu-close" />
+                </div>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">New</a></li>
+                    <li><a href="">Popular</a></li>
+                    <li><a href="">Trending</a></li>
+                    <li><a href="">Categories</a></li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navigation;
